6a946e278eb82dc34361befcefd48b8a1c7fa07a
dmachado <d.grecco.m@gmail.com>
2022-06-09 21:14:26 -0300

    1 file modified

--- src/app/core/cnes.service.ts
+++ src/app/core/cnes.service.ts
@@ -25,3 +25,34 @@ export class CnesService {
     return this.http.get<EstabelecimentoResumo>(this.configUrl);
   }
 }
+
+// lambda AWS
+
+// import json
+// from botocore.vendored import requests
+
+// def lambda_handler(event, context):
+
+//     headers = {'Referer': 'https://cnes.datasus.gov.br'}
+
+//     cnes = 3539717
+
+//     r_estabelecimento = requests.get('https://cnes.datasus.gov.br/services/estabelecimentos?cnes=' + str(cnes), headers=headers)
+//     estabelecimento = r_estabelecimento.json()
+
+//     estabelecimento_id = estabelecimento[0]['id']
+
+//     r_estabelecimento_detalhes = requests.get('https://cnes.datasus.gov.br/services/estabelecimentos/'+ estabelecimento_id, headers=headers)
+//     estabelecimento_detalhe = r_estabelecimento_detalhes.json()
+
+//     print(estabelecimento_detalhe)
+
+//     return {
+//         'statusCode': 200,
+//         'headers': {
+//             'Access-Control-Allow-Headers': 'Content-Type',
+//             'Access-Control-Allow-Origin': '*',
+//             'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+//         },
+//         'body': json.dumps(estabelecimento_detalhe)
+//     }
