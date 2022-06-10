d9589280144cc38f5cbb7ca470b81d2a02e220ef
dmachado <d.grecco.m@gmail.com>
2022-06-10 15:28:37 -0300

    1 file modified

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
-import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
-  imports: [CommonModule, BrowserModule],
+  imports: [CommonModule],
   declarations: [HomeComponent],
 })
 export class PagesModule {}
