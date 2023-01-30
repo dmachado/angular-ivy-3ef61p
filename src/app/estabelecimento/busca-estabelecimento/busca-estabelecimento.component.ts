fb646d46e7066078715fa5994f78ad0a6e043e00
dmachado <d.grecco.m@gmail.com>
2023-01-30 15:53:58 -0300

    4 files modified

--- src/app/estabelecimento/busca-estabelecimento/busca-estabelecimento.component.ts
+++ src/app/estabelecimento/busca-estabelecimento/busca-estabelecimento.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { EstabelecimentoService } from '../estabelecimento.service';
+import { Estabelecimento } from '../estabelecimento';
+
 @Component({
   selector: 'app-busca-estabelecimento',
   templateUrl: './busca-estabelecimento.component.html',
-  styleUrls: ['./busca-estabelecimento.component.css']
+  styleUrls: ['./busca-estabelecimento.component.css'],
 })
 export class BuscaEstabelecimentoComponent implements OnInit {
+  constructor(private estabelecimentoService: EstabelecimentoService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  estabelecimento: Estabelecimento | undefined;
+
+  value = '3348938';
+
+  retrieveEstabelecimento() {
+    this.estabelecimentoService
+      .getEstabelecimento({ cnes: this.value })
+      .subscribe((data: Estabelecimento) => {
+        this.estabelecimento = data;
+        console.log(this.value);
+        console.log(this.estabelecimento);
+      });
   }
-
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { EstabelecimentoService } from '../../estabelecimento/estabelecimento.service';
-import { Estabelecimento } from '../../estabelecimento/estabelecimento';
 
 @Component({
   selector: 'app-home',
@@ -9,21 +7,7 @@ import { Estabelecimento } from '../../estabelecimento/estabelecimento';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private estabelecimentoService: EstabelecimentoService) {}
+  constructor() {}
 
   ngOnInit() {}
-
-  estabelecimento: Estabelecimento | undefined;
-
-  value = '3348938';
-
-  retrieveEstabelecimento() {
-    this.estabelecimentoService
-      .getEstabelecimento({ cnes: this.value })
-      .subscribe((data: Estabelecimento) => {
-        this.estabelecimento = data;
-        console.log(this.value);
-        console.log(this.estabelecimento);
-      });
-  }
 }
