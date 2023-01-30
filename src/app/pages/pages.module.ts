d8f5cf6b849f793ef9eff188dfec771d90a94f5e
dmachado <d.grecco.m@gmail.com>
2023-01-30 15:26:35 -0300

    2 files modified

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
+import { EstabelecimentoModule } from '../estabelecimento/estabalecimento.module';
 
 @NgModule({
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, EstabelecimentoModule],
   declarations: [HomeComponent],
 })
 export class PagesModule {}
